47bea10176bcbc788f404d235e282901857594c5
karam3112 <xkaramx@gmail.com>
2026-03-24 13:29:38 +0200

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -29,13 +29,13 @@
       // مثال:
       // "1.jpg",
       // "2.jpg",
-      // "3.jpg"
-      "4.jpg"
-      "5.jpg"
-      "6.jpg"
-      "7.jpg"
-      "8.jpg"
-      "9.jpg"
+      // "3.jpg",
+      "4.jpg",
+      "5.jpg",
+      "6.jpg",
+      "7.jpg",
+      "8.jpg",
+      "9.jpg",
       "10.jpg"
     ],
     eventRotateSeconds: 20,
